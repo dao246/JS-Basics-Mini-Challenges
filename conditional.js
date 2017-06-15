85bcae57aa98ddf9bb4141b60e323640a61a8fd3
darcy odonnell <darcyanneodonnell@gmail.com>
2017-06-14 21:03:10 -0400

    homework

---
+++ conditional.js
@@ -0,0 +1,179 @@
+
+
+
+var number1 = 5;
+var number2 = 5;
+var add = number1 + number2;
+
+console.log(add);
+
+
+
+
+var music = ["rock", "hiphop", "soul"];
+var places = ["aspen", "santa barbara", "miami"]
+
+var musicAndPlaces = [music, places];
+console.log(musicAndPlaces[0][2]);
+console.log(musicAndPlaces[1][1]);
+
+
+
+var num = 88;
+
+if (num > 100) {
+  alert("is less than 100");
+} else {
+  alert(num + "is greater than 100");
+}
+
+
+
+
+
+function tellMeYourName (yourName) {
+	return console.log("hi " + yourName);
+}
+
+tellMeYourName("darcy");
+
+
+
+function openDoor(doorNumber) {
+  if (doorNumber === 1) {
+    return "puppy";
+  } else if (doorNumber === 2) {
+    return "new pool";
+  } else if (doorNumber === 3) {
+    return "free trip";
+  } else {
+    return "cannot open";
+  }
+}
+
+console.log(openDoor(2));
+console.log(openDoor(3));
+console.log(openDoor(1));
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementsByTagName()[]
+
+// document.getElementsByTagName("h1")[0].addEventListener("click", function(event){  alert('hello')})
+
+
+
+
+
+
+
+// //looops
+
+// for (var i = 10;p 1 >= 10; i--) {
+// 	console.log(i);
+// }
+
+
+
+// family = ["john", "aj", "courtney"];
+
+// var friends
+
+// for(var i = 0; i < friends.length; is my friend) {
+// 	console.log(family [i])
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function doSomeMath( n1, n2, n3 ,n4){
+// 	return n1 + n2 + n3 +n4;
+
+// }
+
+// var result = doSomeMath (1, 4, 6, 8);
+// console.log(result);
+
+
+
+
+// function  alertAge (somePersonsAge) {
+// 	return alert ("your age is" + somePersonsAge);
+
+
+// }
+
+// alertAge ("29");
+
+
+
+// function openThisDoor (doorNum) {
+// 	if (doorNum === 1){
+// 		return alert ("pinapple");
+// 	}
+// 	else if (doorNum === 2) {
+// 		return alert ("orange");
+// 	}
+// 	else if (doorNum === 3) {
+// 		return alert ("apple");
+
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var number = 12;
+
+// if (number < 10) {
+// 	console.log("less than 10");
+// }
+// else {
+// 	console.log("its" + num + "less than ten")
+	
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
